5a683761adcf2379def444a8642c94569a91a185
Muramash <alexandrebroquet.cd@gmail.com>
2022-03-10 21:41:26 +0100

    fix: adjusts & state organization

--- src/components/Paid.js
+++ src/components/Paid.js
@@ -6,37 +6,15 @@ import bolt from './../img/lightning-svgrepo-com.svg'
 // import axios from 'axios';
 import jsonData from './../result.json'
 
-export const Payment = () => {
-    return (
-        <>
-            <div className="empty">
-                 {jsonData.map((payment, key) =>{
-                     return (
-                         <p>{payment.invoiceNumber + ", " + payment.payedDate}</p>
-                     )
-                 })}
-            </div>
-        </>
-    )
-}
 class Paid extends Component {
-    state = {
-            paidPayments: {
-                amount: null,
-                discount: {
-                    rate: null,
-                    maxDaysToPay: null,
-                },
-                invoiceNumber: null,
-                maxDaysToPay: null,
-                multiPaymentStatus: null,
-                payedDate: null,
-                sentDate: null,
-            },
-        }
-      
-      
-    componentDidMount() {
+    constructor() {
+        super();
+        this.state = {
+            allPaidList: []
+        };
+    }
+
+    componentDidMount = async () => {
         //   axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
         //   axios.get('https://app.soan-solutions.io/test_front/datas', {
@@ -58,87 +36,80 @@ class Paid extends Component {
         //     console.log(error.config);
         //   });
 
+        let tempPayment = [];
+        for (let payment of jsonData.payments) {
+            tempPayment.push(payment)
+        }
+        await this.setState({ allPaidList: tempPayment });
 
-      }
+    }
 
     render() {
-        console.log("test: ",jsonData.payments)
-        let allPaidList = []
-        jsonData.payments.forEach(e => {
-            if(e.payedDate != null){
-                allPaidList.push(e);
-            }
-        })
 
-        console.log("here paid : ", allPaidList)
-        
         return (
             <Fragment>
 
-            {allPaidList && allPaidList.map((payment,i) => {
+                {this.state.allPaidList && this.state.allPaidList.map((payment, i) => {
 
-                let fullCases = false;
-                let discountToReduce = ""
-                let discountedPrice = ""
+                    if (!payment.payedDate) return null;
 
-                let formatDateArray = payment.payedDate.split('-')
-                let formatedDate = formatDateArray[2]+"/"+formatDateArray[1]+"/"+formatDateArray[0];
+                    let fullCases = false;
+                    let discountToReduce = ""
+                    let discountedPrice = ""
 
-                console.log(formatedDate);
+                    let formatDateArray = payment.payedDate.split('-')
+                    let formatedDate = formatDateArray[2] + "/" + formatDateArray[1] + "/" + formatDateArray[0];
 
-                if(payment.multiPaymentStatus === "USED" && payment.discount != null) fullCases = true;
+                    if (payment.multiPaymentStatus === "USED" && payment.discount != null) fullCases = true;
 
-                if(payment.discount != null){
-                    discountToReduce = (payment.amount /100) * (payment.discount.rate / 100)
-                    discountedPrice = (payment.amount/100) - discountToReduce
-                }
+                    if (payment.discount != null) {
+                        discountToReduce = (payment.amount / 100) * (payment.discount.rate / 100)
+                        discountedPrice = (payment.amount / 100) - discountToReduce
+                    }
 
-                    return(
+                    return (
                         <Container key={i} className="boxContainer">
                             <Row className="boxRow">
                                 <Col>
-                                <span className="regularText">{payment.invoiceNumber}</span><br/>
-                                <span className="subtitleText">Réglée le {formatedDate}</span>
+                                    <span className="regularText">{payment.invoiceNumber}</span><br />
+                                    <span className="subtitleText">Réglée le {formatedDate}</span>
                                 </Col>
                                 <Col>
-                                {fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/> <span className="regularText">3x et Escompte</span><br/>
-                                </>
+                                    {fullCases &&
+                                        <>
+                                            <img className="boltSVG" alt="" src={bolt} /> <span className="regularText">3x et Escompte</span><br />
+                                        </>
 
-                                }
-                                {payment.multiPaymentStatus === "USED" && !fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/>  <span className="regularText">3x sans frais</span><br/>
-                                </>
-                                }
-                                {payment.multiPaymentStatus === "NONE" && payment.discount != null && !fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/> <span className="regularText">Escompte</span><br/>                                    
-                                </>
-                                }
-                                {payment.multiPaymentStatus === "USED" &&
-                                    <span className="subtitleText_green">Appliqué</span>
-                                }
+                                    }
+                                    {payment.multiPaymentStatus === "USED" && !fullCases &&
+                                        <>
+                                            <img className="boltSVG" alt="" src={bolt} />  <span className="regularText">3x sans frais</span><br />
+                                        </>
+                                    }
+                                    {payment.multiPaymentStatus === "NONE" && payment.discount != null && !fullCases &&
+                                        <>
+                                            <img className="boltSVG" alt="" src={bolt} /> <span className="regularText">Escompte</span><br />
+                                        </>
+                                    }
+                                    {payment.multiPaymentStatus === "USED" &&
+                                        <span className="subtitleText_green">Appliqué</span>
+                                    }
                                 </Col>
                                 <Col>
-                                {payment.discount == null && 
-                                <span className="regularText"> { parseFloat(payment.amount) / 100 } €</span>
-                                } 
-                                {payment.discount != null && 
-                                <>
-                                    <span className="regularText"> { discountedPrice } €</span><br/>
-                                    <span className="dashedPrice"> {parseFloat(payment.amount) / 100 } € </span>
-                                </>
-                                } 
+                                    {payment.discount == null &&
+                                        <span className="regularText"> {parseFloat(payment.amount) / 100} €</span>
+                                    }
+                                    {payment.discount != null &&
+                                        <>
+                                            <span className="regularText"> {discountedPrice} €</span><br />
+                                            <span className="dashedPrice"> {parseFloat(payment.amount) / 100} € </span>
+                                        </>
+                                    }
                                 </Col>
                             </Row>
                         </Container>
                     )
                 })}
-
-
-
             </Fragment>
 
         );
--- src/components/ToPay.js
+++ src/components/ToPay.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Container, Row, Col, Modal, Dropdown } from 'react-bootstrap';
+import { Container, Row, Col, Modal, CloseButton } from 'react-bootstrap';
 import bolt from './../img/lightning-svgrepo-com.svg'
 import lock from "./../img/lock_black_24dp.svg"
 
@@ -8,35 +8,47 @@ import lock from "./../img/lock_black_24dp.svg"
 import jsonData from './../result.json'
 import { Form, Button } from 'react-bootstrap';
 import FormCheckLabel from "react-bootstrap/esm/FormCheckLabel";
+import Select from 'react-select';
 
 class ToPay extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
+            countries: [
+                { label: "France", value: 1 },
+                { label: "Allemagne", value: 2 },
+                { label: "Italie", value: 3 },
+            ],
+            allPaidList: [],
             totalPayCheck: 0,
             show: false,
             selectedCountry: "France"
         };
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
-        this.handleSelect = this.handleSelect.bind(this);
-        
+
     }
 
-    handleSelect = (e) =>{
-        console.log(e);
-        this.setState({ selectedCountry: e })
-      };
 
     showModal = () => {
         this.setState({ show: true });
-      };
-    
-      hideModal = () => {
-        this.setState({ show: false });
-      };
+    };
 
-    componentDidMount() {
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
+    updateCheckedValues = async (e, amount) => {
+        const checked = e.target.checked;
+        if (checked) {
+            await this.setState({ totalPayCheck: this.state.totalPayCheck + amount })
+        } else {
+            await this.setState({ totalPayCheck: this.state.totalPayCheck - amount })
+        }
+    };
+
+
+    componentDidMount = async () => {
         // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
         // axios.get('https://app.soan-solutions.io/test_front/datas', {
@@ -57,37 +69,23 @@ class ToPay extends Component {
         //     }
         //   console.log(error.config);
         // });
+
+        let tempPayment = [];
+        for (let payment of jsonData.payments) {
+            tempPayment.push(payment)
+        }
+        await this.setState({ allPaidList: tempPayment });
+
     }
 
     render() {
-    
-    
-        let totalPayCheck = 0;
-
-        const updateCheckedValues = (e,amount) => {
-            const checked = e.target.checked;
-            if (checked) {
-                totalPayCheck = totalPayCheck + amount
-                document.getElementById("PayCheck").innerHTML = "Payer "+totalPayCheck+" €"
-            } else {
-                totalPayCheck = totalPayCheck - amount
-                document.getElementById("PayCheck").innerHTML = "Payer "+totalPayCheck+" €"
-            }
-          };
-
-        console.log("test: ",jsonData.payments)
-        let allPaidList = []
-        jsonData.payments.forEach(e => {
-            if(e.payedDate === null){
-                allPaidList.push(e);
-            }
-        })
-        console.log("here to pay : ", allPaidList)
 
         return (
             <Fragment>
 
-            {allPaidList && allPaidList.map((payment,i) => {
+                {this.state.allPaidList && this.state.allPaidList.map((payment, i) => {
+
+                    if (payment.payedDate) return null;
 
                     let dueDate = new Date(payment.sentDate);
                     let dueDateString = "";
@@ -98,62 +96,62 @@ class ToPay extends Component {
                     dueDate.setDate(dueDate.getDate() + payment.maxDaysToPay);
                     dueDateString = dueDate.getDay().toString() + "/" + dueDate.getMonth().toString() + "/" + dueDate.getFullYear().toString();
 
-                    if(payment.multiPaymentStatus === "AVAILABLE" && payment.discount != null) fullCases = true;
+                    if (payment.multiPaymentStatus === "AVAILABLE" && payment.discount != null) fullCases = true;
 
-                    if(payment.discount != null){
-                        discountToReduce = (payment.amount /100) * (payment.discount.rate / 100)
-                        discountedPrice = (payment.amount/100) - discountToReduce
+                    if (payment.discount != null) {
+                        discountToReduce = (payment.amount / 100) * (payment.discount.rate / 100)
+                        discountedPrice = (payment.amount / 100) - discountToReduce
                     }
 
-                    return(
+                    return (
                         <Container key={i} className="boxContainer">
                             <Form.Group className="formAdjust" controlId={payment.invoiceNumber}>
-                            <Row className="boxRow">
-                                <Col xs={1}>
-                                {payment.discount == null && 
-                                    <Form.Check onClick={(e) => {updateCheckedValues(e,payment.amount /100)}} type="checkbox"/>
-                                }
-                                {payment.discount != null &&
-                                    <Form.Check onClick={(e) => {updateCheckedValues(e,discountedPrice)}} type="checkbox"/>
-                                }
-                                </Col>
-                                <Col>
-                                    <FormCheckLabel className="regularText" label={payment.invoiceNumber}>{payment.invoiceNumber}</FormCheckLabel><br/>
-                                    <span className="subtitleText">A régler avant le {dueDateString}</span>
-                                </Col>
-                                
-                                <Col>
-                                {fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/> <span className="regularText">3x et Escompte</span><br/>
-                                </>                                
-                                }
-                                {payment.multiPaymentStatus === "AVAILABLE" && !fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/> <span className="regularText">3x sans frais</span><br/>                                    
-                                </>
-                                }
-                                {payment.multiPaymentStatus === "NONE" && payment.discount != null && !fullCases &&
-                                <>
-                                    <img className="boltSVG" src={bolt}/> <span className="regularText">Escompte</span><br/>
-                                </> 
-                                }
-                                {payment.discount != null &&
-                                    <span className="subtitleText_green">-{payment.discount.rate}% pendant {payment.discount.maxDaysToPay} jours.</span>
-                                }
-                                </Col>
-                                <Col>
-                                {payment.discount == null && 
-                                <p className="regularText"> { parseFloat(payment.amount) / 100 } €</p>
-                                } 
-                                {payment.discount != null && 
-                                <>
-                                    <p className="regularText"> { discountedPrice } €</p>
-                                    <span className="dashedPrice"> {parseFloat(payment.amount) / 100 } € </span>
-                                </>
-                                } 
-                                </Col>
-                            </Row>
+                                <Row className="boxRow">
+                                    <Col xs={1}>
+                                        {payment.discount == null &&
+                                            <Form.Check onClick={(e) => { this.updateCheckedValues(e, payment.amount / 100) }} type="checkbox" />
+                                        }
+                                        {payment.discount != null &&
+                                            <Form.Check onClick={(e) => { this.updateCheckedValues(e, discountedPrice) }} type="checkbox" />
+                                        }
+                                    </Col>
+                                    <Col>
+                                        <FormCheckLabel className="regularText" label={payment.invoiceNumber}>{payment.invoiceNumber}</FormCheckLabel><br />
+                                        <span className="subtitleText">A régler avant le {dueDateString}</span>
+                                    </Col>
+
+                                    <Col>
+                                        {fullCases &&
+                                            <>
+                                                <img className="boltSVG" alt="" src={bolt} /> <span className="regularText">3x et Escompte</span><br />
+                                            </>
+                                        }
+                                        {payment.multiPaymentStatus === "AVAILABLE" && !fullCases &&
+                                            <>
+                                                <img className="boltSVG" alt="" src={bolt} /> <span className="regularText">3x sans frais</span><br />
+                                            </>
+                                        }
+                                        {payment.multiPaymentStatus === "NONE" && payment.discount != null && !fullCases &&
+                                            <>
+                                                <img className="boltSVG" alt="" src={bolt} /> <span className="regularText">Escompte</span><br />
+                                            </>
+                                        }
+                                        {payment.discount != null &&
+                                            <span className="subtitleText_green">-{payment.discount.rate}% pendant {payment.discount.maxDaysToPay} jours.</span>
+                                        }
+                                    </Col>
+                                    <Col>
+                                        {payment.discount == null &&
+                                            <p className="regularText"> {parseFloat(payment.amount) / 100} €</p>
+                                        }
+                                        {payment.discount != null &&
+                                            <>
+                                                <p className="regularText"> {discountedPrice} €</p>
+                                                <span className="dashedPrice"> {parseFloat(payment.amount) / 100} € </span>
+                                            </>
+                                        }
+                                    </Col>
+                                </Row>
                             </Form.Group>
                         </Container>
                     )
@@ -161,7 +159,7 @@ class ToPay extends Component {
                 <Container className="validationBox">
                     <Row>
                         <Col>
-                            <Button className="buttonStyle" id="PayCheck" onClick={this.showModal}>Payer {totalPayCheck} €</Button>
+                            <Button className="buttonStyle" id="PayCheck" onClick={this.showModal}>Payer {this.state.totalPayCheck} €</Button>
                         </Col>
                     </Row>
                 </Container>
@@ -169,27 +167,28 @@ class ToPay extends Component {
                 <Container>
                     <Row>
                         <Col className="subtitleBlock">
-                            <img className="lockSVG" src={lock}/><span className="subtitleBottom">Paiement en ligne 100 % sécurisé</span>
+                            <img className="lockSVG" alt="" src={lock} /><span className="subtitleBottom">Paiement en ligne 100 % sécurisé</span>
                         </Col>
                     </Row>
                 </Container>
-                
-                <Modal 
-                    show={this.state.show} 
+
+                <Modal
+                    show={this.state.show}
                     handleClose={this.hideModal}
                     aria-labelledby="contained-modal-title-vcenter"
                     centered
                 >
-                    <Modal.Header closeButton onClick={this.hideModal}>
-                            <Modal.Title id="contained-modal-title-vcenter" class="modalTitle">
-                                Paiment sécurisé par prélèvement bancaire
-                            </Modal.Title>
+                    <Modal.Header >
+                        <Modal.Title id="contained-modal-title-vcenter" class="modalTitle">
+                            Paiment sécurisé par prélèvement bancaire
+                        </Modal.Title>
+                        <CloseButton onClick={this.hideModal}/>
                     </Modal.Header>
                     <Modal.Body>
                         <Container>
                             <Row>
                                 <Col className="modalBox">
-                                <p className="mandatText">Mise en place d’un mandat SEPA MANGOPAY</p>
+                                    <p className="mandatText">Mise en place d’un mandat SEPA MANGOPAY</p>
                                 </Col>
                             </Row>
                             <Row className="p-2">
@@ -221,17 +220,7 @@ class ToPay extends Component {
                                 </Col>
                                 <Col>
                                     <Form.Label className="subtitleForm">Pays <span className="redText">*</span></Form.Label>
-                                    <Dropdown>
-                                        <Dropdown.Toggle className="dropdownForm" id="dropdown-basic">
-                                            {this.state.selectedCountry}
-                                        </Dropdown.Toggle>
-
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item eventKey="France">France</Dropdown.Item>
-                                            <Dropdown.Item eventKey="Allemagne">Allemagne</Dropdown.Item>
-                                            <Dropdown.Item eventKey="Italie">Italie</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
+                                    <Select className="dropdownForm" options={this.state.countries} />
                                 </Col>
                             </Row>
                             <Row className="p-2">
@@ -239,12 +228,12 @@ class ToPay extends Component {
                                     <Form.Label className="subtitleForm">IBAN <span className="redText">*</span></Form.Label>
                                     <Form.Control id="iban" className="inputForm" type="text" placeholder="____ ____ ____ ____ ____ ____ ___" />
                                 </Col>
-                            </Row>                            
+                            </Row>
                         </Container>
                     </Modal.Body>
                     <Modal.Footer>
                         <a className="backButton" onClick={this.hideModal}>Annuler</a>
-                        <Button className="buttonStyle" onClick={this.hideModal}>Payer {totalPayCheck} €</Button>
+                        <Button className="buttonStyle" onClick={this.hideModal}>Payer {this.state.totalPayCheck} €</Button>
                     </Modal.Footer>
                 </Modal>
             </Fragment>
